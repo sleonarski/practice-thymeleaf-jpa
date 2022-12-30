ddcc72d819e724d006acfca2fb4913f75ee57679
Sebastian <sed@interia.eu>
2022-12-30 15:53:54 +0100

    Initial commit #1

---
+++ src/main/java/pl/test/practicethymeleafjpa/PracticeThymeleafJpaApplication.java
@@ -0,0 +1,13 @@
+package pl.test.practicethymeleafjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeThymeleafJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticeThymeleafJpaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/test/practicethymeleafjpa/PracticeThymeleafJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.test.practicethymeleafjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeThymeleafJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
